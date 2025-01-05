9a07d49f8a14b46fe794a0ef6f4fd210691bf253
umamule <umule33@gmail.com>
2025-01-05 15:29:11 +0530

    musicservice

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 /**
  * @format
  */
+import { playbackService } from './musicPalyerServise';
+import TrackPlayer from 'react-native-track-player';
 
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(...);
+TrackPlayer.registerPlaybackService(() => playbackService);
